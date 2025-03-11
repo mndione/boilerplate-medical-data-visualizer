8b58f071d4839a89ab6909bfb98a54a2d803b7d9
Mamadou Ndione <mndione@gmail.com>
2025-03-11 14:45:25 +0000

    feat: catplot done

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -4,30 +4,35 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # 1
-df = None
-
+df = pd.read_csv('medical_examination.csv')
+#print(df.head())
 # 2
-df['overweight'] = None
+df['overweight'] = np.where((df['weight'] / (df['height'] / 100)**2) > 25, 1, 0) 
+#print(df.head())
 
 # 3
-
+df['cholesterol'] = np.where(df['cholesterol'] == 1, 0, 1)
+df['gluc'] = np.where(df['gluc'] == 1, 0, 1)
 
 # 4
 def draw_cat_plot():
     # 5
-    df_cat = None
-
+    df_cat = pd.melt(df, id_vars=['cardio'], value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'], value_name = 'wellness', var_name = 'indicator')
+    #print(df_cat)
 
     # 6
-    df_cat = None
-    
+    df_cat = df_cat.groupby(['cardio', 'indicator', 'wellness']).value_counts().reset_index()
+    df_cat.rename(columns = {0: 'total'}, inplace=True)
+    #print(df_cat)
+    #return 
 
     # 7
-
+    #df_cat['total'].dtype = np.long
+    #sns.catplot(data = df_cat, x="cardio", y = "wellness", col="indicator", kind="count")
 
 
     # 8
-    fig = None
+    fig = sns.catplot(data=df_cat, y="total",  x="indicator", hue="wellness", col="cardio")
 
 
     # 9
